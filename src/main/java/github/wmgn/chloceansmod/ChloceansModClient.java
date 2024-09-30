6f401883d4a63eb21d0f1ee40f2ffeeb7f2c08c1
wmgn <willdmorg@gmail.com>
2024-09-29 22:12:07 -0400

    Initial commit

---
+++ src/main/java/github/wmgn/chloceansmod/ChloceansModClient.java
@@ -0,0 +1,10 @@
+package github.wmgn.chloceansmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChloceansModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/github/wmgn/chloceansmod/ChloceansModDataGenerator.java
@@ -0,0 +1,11 @@
+package github.wmgn.chloceansmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChloceansModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
